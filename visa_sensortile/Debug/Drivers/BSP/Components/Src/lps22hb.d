c1ec907a0df1a18d8b072b8b820ff859b24df59b
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2021-11-18 11:51:35 -0500

    Remove debug folder

--- visa_sensortile/Debug/Drivers/BSP/Components/Src/lps22hb.d
+++
@@ -1,8 +0,0 @@
-Drivers/BSP/Components/Src/lps22hb.o: \
- ../Drivers/BSP/Components/Src/lps22hb.c \
- /Users/stephensheldon/STM32CubeIDE/visa_sensortile/visa_sensortile/Drivers/BSP/Components/Inc/lps22hb.h \
- /Users/stephensheldon/STM32CubeIDE/visa_sensortile/visa_sensortile/Drivers/BSP/Components/Inc/lps22hb_reg.h
-
-/Users/stephensheldon/STM32CubeIDE/visa_sensortile/visa_sensortile/Drivers/BSP/Components/Inc/lps22hb.h:
-
-/Users/stephensheldon/STM32CubeIDE/visa_sensortile/visa_sensortile/Drivers/BSP/Components/Inc/lps22hb_reg.h:
--- visa_sensortile/Debug/Drivers/BSP/Components/Src/lps22hb_reg.d
+++
@@ -1,5 +0,0 @@
-Drivers/BSP/Components/Src/lps22hb_reg.o: \
- ../Drivers/BSP/Components/Src/lps22hb_reg.c \
- /Users/stephensheldon/STM32CubeIDE/visa_sensortile/visa_sensortile/Drivers/BSP/Components/Inc/lps22hb_reg.h
-
-/Users/stephensheldon/STM32CubeIDE/visa_sensortile/visa_sensortile/Drivers/BSP/Components/Inc/lps22hb_reg.h:
